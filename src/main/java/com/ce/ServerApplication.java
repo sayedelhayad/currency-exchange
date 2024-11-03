b95f7c7d3fd4c0e9b3a86506d36b1c7543873026
Sayed Abdelhafez <sayed.elhayad@gmail.com>
2024-11-03 22:18:00 +0200

    enable in-memory caching foe exchange rates api

--- src/main/java/com/ce/ServerApplication.java
+++ src/main/java/com/ce/ServerApplication.java
@@ -3,10 +3,11 @@ package com.ce;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @Slf4j
+@EnableCaching
 public class ServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ce/bill/service/impl/ExchangeRateClientImpl.java
+++ src/main/java/com/ce/bill/service/impl/ExchangeRateClientImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cache.annotation.Cacheable;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +19,9 @@ public class ExchangeRateClientImpl implements ExchangeRateClient {
     private final Gson gson;
 
     @Override
+    @Cacheable(value = "exchangeRates",
+            key = "#originalCurrency",
+            unless = "#result == null || #result.result != 'success'")
     public ExchangeRateResponse getExchangeRate(final String originalCurrency) {
 
         String fullUrl = url.replace("{original-currency}", originalCurrency);
